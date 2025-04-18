bb87276bacc42702deab7c4ffd9cc3acbdb9b1a3
Aryan <aryan.agg72@gmail.com>
2025-04-19 04:30:28 +0530

    Update Footer.tsx

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -34,11 +34,11 @@ const Footer = () => {
                 <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
                     <div className="font-bold text-[16px]">Social Media</div>
                     <p className="flex flex-row items-center my-[15px] cursor-pointer">
-                        <RxInstagramlogo />
+                        <RxInstagramLogo />
                         <span className="text-[15px] ml-[6px]">Instagram</span>    
                     </p>
                     <p className="flex flex-row items-center my-[15px] cursor-pointer">
-                        <RxTwiterLogo />
+                        <RxTwitterLogo />
                         <span className="text-[15px] ml-[6px]">Twitter</span>    
                     </p>
                     <p className="flex flex-row items-center my-[15px] cursor-pointer">
